26fd38d34f8ef5c4679b2a8a1eff9210bb68994f
andrewdonaldsylvester <35876206+andrewdonaldsylvester@users.noreply.github.com>
2019-07-17 15:18:13 -0400

    Working Neural Net Class

--- Neural_Network.py
+++ Neural_Network.py
@@ -3,68 +3,64 @@ import numpy as np
 class NeuralNet(object):
 
 
-    def __init__(self, W1, W2, activation, bias):
-        self.W1 = W1 # first weight matrix
-        self.W2 = W2 # second weight matrix
-        self.activation = activation # activation function (usually step)
-        self.bias = bias 
-        if self.activation == 'step': # sets activation function to the right function
+    def __init__(self, num_input, num_hidden, num_output, activation):
+        self.w1 = np.random.rand(num_input, num_hidden)
+        self.w2 = np.random.rand(num_hidden, num_output)
+
+        self.b1 = np.random.randint(0,100)
+        self.b2 = np.random.randint(0,100)
+
+        self.h1 = np.ones((num_hidden, ))
+        self.h2 = np.ones((num_output, ))
+
+        self.activation = activation
+
+        if self.activation == 'step':
             self.activation_function = self.step
         elif self.activation == 'sigmoid':
             self.activation_function = self.sigmoid
         else:
-            raise Exception('The given activation function is not a known function')
+            raise Exception('the given activation function is not a known function')
 
 
     def step(self, a):
         stepped_a = np.zeros(np.shape(a))
+
         if a.ndim == 1:
             for x_dim in range(np.shape(a)[0]):
                 if a[x_dim] <= 0:
                     stepped_a[x_dim] = 0
                 elif a[x_dim] > 0:
                     stepped_a[x_dim] = 1
-        elif a.ndim == 2:
-            for y_dim in range(np.shape(a)[0]):
-                for x_dim in range(np.shape(a)[1]):
-                    if a[y_dim][x_dim] <= 0:
-                        stepped_a[y_dim][x_dim] = 0
-                    elif a[y_dim][x_dim] > 0:
-                        stepped_a[y_dim][x_dim] = 1
+        else:
+            raise Exception('input is not a vector')
+
         return stepped_a
 
 
     def sigmoid(self, a):
         sigmoid_a = np.zeros(np.shape(a))
+
         if a.ndim == 1:
             for x_dim in range(np.shape(a)[0]):
                 sigmoid_a = 1/(1+(np.exp(a[x_dim])))
-        elif a.ndim == 2:
-            for y_dim in range(np.shape(a)[0]):
-                for x_dim in range(np.shape(a)[1]):
-                    sigmoid_a = 1 / (1 + (np.exp(a[y_dim][x_dim])))
+        else:
+            raise Exception('input is not a vector')
+
         return sigmoid_a
 
 
-    def compute(self, nodes):
-        self.hidden_nodes = np.ones((np.shape(nodes)[0]+1, ))
-        self.hidden_nodes[:-1] = np.multiply(nodes)
-        self.hidden_nodes[-1:] = self.bias
+    def compute(self, h0):
+        self.h1[:-1] = np.multiply(h0, self.w1)
+        self.h1[-1:] = self.b1
+
+        self.h2[:-1] = np.multiply(self.activation_function(self.h1), self.w2)
+        self.h2[-1:] = self.b2
+
+        return self.h2
 
 
-
-W1 = np.array([[1,1],
-               [1,1],
-               [-1,0]])
-
-W2 = np.array([[-1],
-                [1],
-                [-1]])
-
-xor = NeuralNet(W1,W2,'step')
+xor = NeuralNet(3,3,1,'step')
 
 
-nodes = np.array([[1,0,1]])
-
-
-print(xor.compute(nodes))
+print('W1 is \n \n {} \n \n W2 is \n \n {} \n \n The 1st bias is {} \n \n The 2nd bias is {}'.format(xor.w1, xor.w2, xor.b1, xor.b2))

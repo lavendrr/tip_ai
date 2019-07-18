517ce4f676558d32993e0215da724756caf585e0
Wolfalamew <45954191+Wolfalamew@users.noreply.github.com>
2019-07-18 15:20:08 -0400

    Fixed/Finished Pixel Search and List Function
    Updated the "pixel" function to be able to fully search and add dark pixels into the list "p".

--- pixelfunction.py
+++ pixelfunction.py
@@ -4,14 +4,16 @@ from read_image import *
 img = read_image.image
 p = []
 
+def pixels:
+    for i in img:
+        for j in i:
+                if i <= 100:
+                    p += i
+                else:
+                    continue
+
 class PixelFunct(object):
     def __init__(self)
-        for i in img:
-            if i == 1:
-                p += i
-            else:
-                continue
-
         for i in range(p):
             if i != range(p)[-1]:
                 if p[i] == p[i+1]
